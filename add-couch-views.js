e2c2dd8b5629032f192e507059e266caef15b638
Tom Hutchinson <git@tomisme.com>
2014-09-22 11:43:59 +0800

    add error reporting

--- add-couch-views.js
+++ add-couch-views.js
@@ -1,3 +1,4 @@
+var prog   = require('commander');
 var cradle = require('cradle');
 var db = new(cradle.Connection)().database('locations');
 
@@ -5,6 +6,13 @@ db.save('_design/locations', {
   all: {
     map: function(doc) {
       if (doc.name) { emit(doc.name, doc); }
+    }
+  },
+  function(err, res) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(res);
     }
   }
 });
