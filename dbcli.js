93dc3cd8eb8bd95da1ec0ce910df5297b1913d28
Tom Hutchinson <git@tomisme.com>
2014-09-22 11:33:59 +0800

    add production support for dbcli.js

--- dbcli.js
+++ dbcli.js
@@ -3,9 +3,11 @@
 const
   request = require('request'),
 
+  url = process.env.COUCH_URL || '127.0.0.1:5984';  
+
   options = {
     method: process.argv[2] || 'GET',
-    url: 'http://localhost:5984/' + (process.argv[3] || '')
+    url: url + '/' + (process.argv[3] || '')
   };
 
 request(options, function(err, res, body) {
